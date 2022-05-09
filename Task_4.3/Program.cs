dad234019a063f9cb2c27e626d13ca2186b20d16
Renata <myska2@yandex.ru>
2022-05-09 08:21:36 +0300

    t4.3

---
+++ Task_4.3/Program.cs
@@ -0,0 +1,22 @@
+﻿//напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] array = new int[8];
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for (int index = 0; index < length; index++)
+    {
+        collection[index] = new Random().Next(-1, 1000);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++)
+    {
+        Console.WriteLine(col[position]);
+    }
+}
+
+FillArray(array);
+PrintArray(array);
