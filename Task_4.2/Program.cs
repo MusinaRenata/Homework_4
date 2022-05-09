0163d33c84282f88839e57a2c28ff86b65c4be37
Renata <myska2@yandex.ru>
2022-05-09 08:20:38 +0300

    t4.2

---
+++ Task_4.2/Program.cs
@@ -0,0 +1,27 @@
+﻿//напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+
+int GetValue(string text)
+{
+    Console.Write($"{text} ");
+string GetValueStr=Console.ReadLine()!;
+int value=Convert.ToInt32(GetValueStr);
+return value;
+}
+
+double GetSum(int a)
+{ 
+    double sum = 0;
+    while (a != 0)
+    {
+        sum += a % 10;
+        a = a / 10;
+    }
+    return sum;
+}
+
+
+int N = GetValue("N= ");
+
+double result= GetSum(N);
+Console.WriteLine(result);
